c8be9f03b68386a2c94ea2b162c2b76141dd6ba8
Zaur Bairamau <bairamau.z@gmail.com>
2024-05-24 04:37:13 +0400

    Add native-like scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,13 @@ import { getProducts, getCategories, getProductById } from '@/services/ecwid'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
